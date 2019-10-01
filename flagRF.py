d1d9d311e0a107711e691522a7fe47162169e713
DanilinAlexandr <danilin@yandexlyceum.ru>
2019-10-01 16:27:21 +0200

    флагшток

--- flagRF.py
+++ flagRF.py
@@ -2,19 +2,19 @@ import pygame
 
 pygame.init()
 
-size = width, height = 300, 300
+size = width, height = 600, 400
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Флаг России")
-screen.fill((255, 255, 255))
+screen.fill((117, 187, 253))
 
 
 def draw_flagshtok():
     rect_color = pygame.Color("black")
     rect_width = 0
-    rect_point = [(20, 10), (20, 280)]
+    rect_point = [(20, 10), (2, 280)]
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
-    pygame.draw.rect(screen, rect_color, [(10, 270),(30,280)], rect_width)
+    pygame.draw.rect(screen, rect_color, [(10, 270),(50, 20)], rect_width)
 
 
 draw_flagshtok()
