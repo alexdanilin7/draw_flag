04897aee4e18c55c23baddf77f80efdc2363aaff
DanilinAlexandr <danilin@yandexlyceum.ru>
2019-10-01 16:36:33 +0200

    Белый прямоугольник

--- flagRF.py
+++ flagRF.py
@@ -16,8 +16,15 @@ def draw_flagshtok():
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
     pygame.draw.rect(screen, rect_color, [(10, 270),(50, 20)], rect_width)
 
+def draw_WhiteRect():
+    rect_color = pygame.Color("white")
+    rect_width = 0
+    rect_point = [(22, 10), (100, 30)]
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
 
 draw_flagshtok()
+draw_WhiteRect()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
