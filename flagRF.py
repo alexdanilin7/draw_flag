24587be4605d8d5ac66f6140a364f51432cb73ef
DanilinAlexandr <danilin@yandexlyceum.ru>
2019-10-02 10:50:16 +0200

    синий

--- flagRF.py
+++ flagRF.py
@@ -16,8 +16,13 @@ def draw_flagshtok():
     pygame.draw.rect(screen, rect_color, rect_point, rect_width)
     pygame.draw.rect(screen, rect_color, [(10, 270),(50, 20)], rect_width)
 
-
+def draw_BlueRect():
+    rect_color = pygame.Color("blue")
+    rect_width = 0
+    rect_point = [(22, 40), (100, 30)]
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
 draw_flagshtok()
+draw_BlueRect()
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
